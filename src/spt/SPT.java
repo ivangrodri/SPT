c566216103de815b1ea508004ec09f08d737f49e
Ivan Rodriguez <rodrivan07@gmail.com>
2022-08-14 20:54:47 -0400

    Interfaz de constantes de materiales interactiva

---
+++ src/spt/SPT.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spt;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author rodri
+ */
+public class SPT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         pantallaInicio z = new pantallaInicio();
+        z.setVisible(true);
+
+    }
+    
+}
